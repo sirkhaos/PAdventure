3afd8a15bd3bc8cad73e78e790163b0af625507a
sirkhaos <alberto.torres.carreno@gmail.com>
2019-04-11 19:34:42 -0400

    primera version del control del jugador y animaciones

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 4.0f;
+
+    private string horizontal = "Horizontal";
+    private string vertical = "Vertical";
+
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mathf.Abs(Input.GetAxisRaw(horizontal)) > 0.5f)
+        {
+            this.transform.Translate(new Vector3(Input.GetAxisRaw(horizontal) * speed * Time.deltaTime, 0, 0));
+        }
+        if (Mathf.Abs(Input.GetAxisRaw(vertical)) > 0.5f)
+        {
+            this.transform.Translate(new Vector3(0, Input.GetAxisRaw(vertical) * speed * Time.deltaTime, 0));
+        }
+        animator.SetFloat(horizontal, Input.GetAxisRaw(horizontal));
+        animator.SetFloat(vertical, Input.GetAxisRaw(vertical));
+    }
+}
