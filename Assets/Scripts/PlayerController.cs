e8571ca5b15acf9b63b2ba9c3bad47f9e71e0c29
sirkhaos <alberto.torres.carreno@gmail.com>
2019-04-13 00:45:30 -0400

    movimiento del jugador

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,10 +7,16 @@ public class PlayerController : MonoBehaviour
 
     public float speed = 4.0f;
 
-    private string horizontal = "Horizontal";
-    private string vertical = "Vertical";
+    private const string horizontal = "Horizontal";
+    private const string vertical = "Vertical";
+    private const string lastHorizontal = "LastHorizontal";
+    private const string lastVertical = "LastVertical";
+    private const string walkingState = "Walking";
 
     private Animator animator;
+
+    private bool walking = false;
+    private Vector2 lastMovement = Vector2.zero;
 
     // Start is called before the first frame update
     void Start()
@@ -21,15 +27,23 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        walking = false;
         if (Mathf.Abs(Input.GetAxisRaw(horizontal)) > 0.5f)
         {
             this.transform.Translate(new Vector3(Input.GetAxisRaw(horizontal) * speed * Time.deltaTime, 0, 0));
+            walking = true;
+            lastMovement = new Vector2(Input.GetAxisRaw(horizontal), 0);
         }
         if (Mathf.Abs(Input.GetAxisRaw(vertical)) > 0.5f)
         {
             this.transform.Translate(new Vector3(0, Input.GetAxisRaw(vertical) * speed * Time.deltaTime, 0));
+            walking = true;
+            lastMovement = new Vector2(0, Input.GetAxisRaw(vertical));
         }
         animator.SetFloat(horizontal, Input.GetAxisRaw(horizontal));
         animator.SetFloat(vertical, Input.GetAxisRaw(vertical));
+        animator.SetBool(walkingState, walking);
+        animator.SetFloat(lastHorizontal, lastMovement.x);
+        animator.SetFloat(lastVertical, lastMovement.y);
     }
 }
