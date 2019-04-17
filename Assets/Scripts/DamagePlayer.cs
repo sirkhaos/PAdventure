d5b82ad6056c44c555ca8d00a1a309e1ee00f8f1
sirkhaos <alberto.torres.carreno@gmail.com>
2019-04-17 19:11:35 -0400

    creacion de manager de vida y creando el sistema de daño al jugador

--- Assets/Scripts/DamagePlayer.cs
+++ Assets/Scripts/DamagePlayer.cs
@@ -4,41 +4,14 @@ using UnityEngine;
 
 public class DamagePlayer : MonoBehaviour
 {
-    public float timeToRevivePlayer;
-    private float timeRevivalCounter;
-    private bool playerReviving;
+    public int damage;
 
-    private GameObject thePlayer;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (playerReviving)
-        {
-            timeRevivalCounter -= Time.deltaTime;
-            if (timeRevivalCounter < 0)
-            {
-                playerReviving = false;
-                thePlayer.SetActive(true);
-            }
-        }
-    }
+    
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag.Equals("Player"))
         {
-            //colision establesida entre player y enemigo
-            collision.gameObject.SetActive(false);
-            playerReviving = true;
-            timeRevivalCounter = timeToRevivePlayer;
-            thePlayer = collision.gameObject;
-            
+            collision.gameObject.GetComponent<HealthManager>().damgeCharacter(damage);
         }
     }
 }
---
+++ Assets/Scripts/HealthManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    public int maxHealth;
+
+    [SerializeField]
+    private int currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentHealth <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+    public void damgeCharacter(int damage)
+    {
+        currentHealth -= damage;
+    }
+
+    public void updateMaxhelth(int newMaxHelth)
+    {
+        maxHealth = newMaxHelth;
+        currentHealth = maxHealth;
+    }
+}
