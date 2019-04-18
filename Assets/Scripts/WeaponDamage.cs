1ae4736fe17a34a813e9a98bfafc9522a81fb4a2
sirkhaos <alberto.torres.carreno@gmail.com>
2019-04-18 13:35:23 -0400

    asignando daño al arma

--- Assets/Scripts/WeaponDamage.cs
+++ Assets/Scripts/WeaponDamage.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class WeaponDamage : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int damage;
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.tag.Equals("Enemy"))
+        {
+            collision.gameObject.GetComponent<HealthManager>().damgeCharacter(damage);
+        }
     }
 }
