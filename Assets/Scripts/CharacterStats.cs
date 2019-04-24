d3df7cdf623f26896383fbcd2d6367d3149a1e17
sirkhaos <alberto.torres.carreno@gmail.com>
2019-04-24 18:05:16 -0400

    reparar con la version de .net a 4x para solucionar problema de textMesh

---
+++ Assets/Scripts/CharacterStats.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+    public int currentLevel;
+    public int currentExp;
+    public int[] expToLevelUp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentLevel >= expToLevelUp.Length)
+        {
+            return;
+        }
+
+        if (currentExp >= expToLevelUp[currentLevel - 1])
+        {
+            currentLevel++;
+        }
+    }
+    public void AddExp(int exp)
+    {
+        currentExp += exp;
+    }
+}
