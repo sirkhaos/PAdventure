63c59bc6efdbf2fd6ca9202435fad3e9494d6ca1
sirkhaos <alberto.torres.carreno@gmail.com>
2019-05-02 15:30:57 -0400

    agregando el script de control del volumen general

---
+++ Assets/Scripts/AudioVolumeManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioVolumeManager : MonoBehaviour
+{
+    private AudioVolumeController[] audios;
+    public float maxVolumeLevel, currentVolumeLevel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audios = FindObjectsOfType<AudioVolumeController>();
+        ChangeGlobalAudioVolumen();
+    }
+
+    public void ChangeGlobalAudioVolumen()
+    {
+        if (currentVolumeLevel >= maxVolumeLevel)
+        {
+            currentVolumeLevel = maxVolumeLevel;
+        }
+        foreach(AudioVolumeController avc in audios)
+        {
+            avc.setAudioLevel(currentVolumeLevel);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeGlobalAudioVolumen();
+    }
+}
