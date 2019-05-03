69995a98b27dd80b0f1111b92a2ca57db829f539
sirkhaos <alberto.torres.carreno@gmail.com>
2019-05-03 01:03:46 -0400

    terminada la creacion de monedas y el metodo de almasenamiento y consumo de esta

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public int value;
+    private CoinsManager manager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = FindObjectOfType<CoinsManager>();
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            manager.AddMoney(value);
+            Destroy(gameObject);
+        }
+    }
+}
